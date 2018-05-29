65c27ab16ed931d67a3f13b05c0b9241116f95b5
Davide <davide.iaiunese@gmail.com>
2018-05-29 11:33:10 +0200

    First commit

---
+++ App.js
@@ -0,0 +1,84 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+	Platform,
+	StyleSheet,
+	Text,
+	View,
+ 	Animated,
+	Image, 
+	Easing,
+	TouchableHighlight
+} from 'react-native';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+	
+	state = {
+		spinValue: new Animated.Value(0),
+		touched: false,
+	}
+
+	spin () {
+		this.setState((prevState) => ({
+			touched: !prevState.touched
+		}))
+		let spinning = this.state.spinValue.setValue(0);
+		// Setup animation
+		Animated.timing(
+			this.state.spinValue,
+			{
+				toValue: this.state.touched? 1 : 0,
+				duration: 2000,
+				easing: Easing.linear,
+			}
+		).start();
+	}
+
+	render() {
+		
+		// Interpolate beginning and end values
+		let spin = this.state.spinValue.interpolate({
+			inputRange: [0, 1],
+			outputRange: ['0deg', '360deg']
+		})
+		console.log(this.state.spinValue);
+		
+		return (
+		  <View style={styles.container}>
+			<TouchableHighlight onPress={() => this.spin()}>				
+				<Animated.Image
+					source={require('./placeholder.png')}
+					style={{
+						width: 200,
+						height: 200, 
+						borderRadius: 100,
+						transform: [{rotate: spin}] 
+					}}
+				/>
+			</TouchableHighlight>
+		  </View>
+		);
+	}
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#F5FCFF',
+	}
+});
